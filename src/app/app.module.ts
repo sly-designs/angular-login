cff4339a741aa3a3b90777b0475c22e4f3f1f728
slydesigns <sylvestorsielei@gmail.com>
2023-01-05 22:31:22 +0300

    login form done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginComponent } from './login/login.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +11,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +27,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatButtonModule,
     MatCardModule,
     MatInputModule,
+    MatIconModule,
     ReactiveFormsModule
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators , FormBuilder} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -8,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit{
 
-  constructor() { }
+  loginForm! : FormGroup;
+  hide: boolean = false;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    
+    this.loginForm = new FormGroup(
+      {
+        email : new FormControl('', [Validators.required, Validators.email]),
+        password : new FormControl('', [Validators.required, Validators.minLength(6)])
+      }
+    );
   }
 
-}
+  onLogin(){
+    if (!this.loginForm.valid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
+  }
+
